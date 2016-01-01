4db53519da2963b51797a0d6bbe16f6c96dfd019
Andrew <asolesa@yahoo.ca>
2016-01-01 11:50:17 -0500

    meh
    meh

--- musique libre/Properties/Resources.Designer.cs
+++ musique libre/Properties/Resources.Designer.cs
@@ -113,29 +113,9 @@ namespace musique_libre.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap next {
-            get {
-                object obj = ResourceManager.GetObject("next", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap play {
             get {
                 object obj = ResourceManager.GetObject("play", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap prev {
-            get {
-                object obj = ResourceManager.GetObject("prev", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
